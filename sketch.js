5721b686e279d1ae17d737e64b6c21525b5e5eb7
hkr005078 <80819216+hkr005078@users.noreply.github.com>
2021-03-17 17:43:52 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://hkr005078.github.io/guaguagua/model.json';
 
 
 function preload() {
